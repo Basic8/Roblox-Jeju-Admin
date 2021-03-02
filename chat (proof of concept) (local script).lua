27c0e8e7af643e132270e3792af177f71739a783
Basic88 <79813647+Basic8@users.noreply.github.com>
2021-03-02 12:17:30 -0800

    Update and rename hello world.lua to chat (proof of concept) (local script).lua

---
+++ chat (proof of concept) (local script).lua
@@ -0,0 +1,8 @@
+--IS IN A LOCAL SCRIPT
+local player = game.Players.LocalPlayer --LocalPLayer
+local commands = {"oof"} --Will improve
+local prefix = "!" --Prefix for admin
+
+player.Chatted:Connect(function(msg) --Player chatted? We will se what
+--Will add code later    
+end)
--- hello world.lua
+++
@@ -1,1 +0,0 @@
-print("Hello World") --Quick note that I'm new to github! This was just a test.
