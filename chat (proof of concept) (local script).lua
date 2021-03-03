0988816035b4a3096474d80791c7a63bc107db13
Basic88 <79813647+Basic8@users.noreply.github.com>
2021-03-02 16:51:23 -0800

    Update chat (proof of concept) (local script).lua

--- chat (proof of concept) (local script).lua
+++ chat (proof of concept) (local script).lua
@@ -1,8 +1,14 @@
---IS IN A LOCAL SCRIPT
-local player = game.Players.LocalPlayer --LocalPLayer
-local commands = {"oof"} --Will improve
-local prefix = "!" --Prefix for admin
+local player = game.Players.LocalPlayer
+local prefix = "!"
+local commands = {"oof"} 
 
-player.Chatted:Connect(function(msg) --Player chatted? We will se what
---Will add code later    
+player.Chatted:Connect(function(msg) 
+	if string.sub(msg,1,1) == "!" then 
+		print("is admin command")
+		for i,item in pairs(commands) do
+			if string.sub(msg,2,#item+1) == item then
+				print("oof")
+			end
+		end
+	end
 end)
